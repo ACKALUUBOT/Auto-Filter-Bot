29173f7d5c0603b6a58cbac40d22fc6139fc4fab
ACKALUUBOT <ackaluubotz@gmail.com>
2026-04-06 19:29:54 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from info import INDEX_CHANNELS, INDEX_EXTENSIONS
 from database.ia_filterdb import save_file
 
-media_filter = filters.document | filters.video
+media_filter = filters.document | filters.video | filters.audio
 
 
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
